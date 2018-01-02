0b6f68b43dccec2cb4f6eacae26b0d56b577df5f
jyang <jingoriver@foxmail.com>
2018-01-02 22:46:42 +0800

    examples of book, sheet, cell objects for excel

--- simple.py
+++ simple.py
@@ -3,10 +3,10 @@ from xlrd import open_workbook
 wb = open_workbook('simple.xls')
 
 for s in wb.sheets():
-    print 'Sheet:', s.name
+    print('Sheet:', s.name)
     for row in range(s.nrows):
         values = []
         for col in range(s.ncols):
             values.append(s.cell(row,col).value)
-            print ','.join(values)
-        print
+            print(','.join(values))
+        print()
